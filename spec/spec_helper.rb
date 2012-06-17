3c27932c013189072ee72adc323ba9628d66d80f
John Allen <john@threedogconsulting.com>
2012-06-17 18:55:32 -0400

    adds test framework

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'turn/autorun'
+require 'vcr'
+
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/schedule-scraper'))
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.hook_into :fakeweb
+end
